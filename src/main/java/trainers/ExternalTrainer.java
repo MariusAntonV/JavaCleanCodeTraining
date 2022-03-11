62208bd17df58385a0ff748ff9da82eeaa0951ad
manton <marius-vasile.anton@frequentis.com>
2022-03-11 09:54:28 +0200

    Break Interface Segregation Principle

---
+++ src/main/java/trainers/ExternalTrainer.java
@@ -0,0 +1,32 @@
+/************************************************************************
+ ** COPYRIGHT: FREQUENTIS AG. All rights reserved.
+ **            Registered with Commercial Court Vienna,
+ **            reg.no. FN 72.115b.
+ ************************************************************************/
+package trainers;
+
+import training.Trainer;
+
+/**
+ *
+ * @author MAnton
+ */
+public class ExternalTrainer implements Trainer
+{
+   @Override
+   public String getName()
+   {
+      return null;
+   }
+
+
+   @Override
+   public double deliverExperience( int difficulty )
+   {
+      return difficulty * 2;
+   }
+
+   public void pay(int cost){
+      //adding this method to Trainer interface will break Interface Segregation
+   }
+}
